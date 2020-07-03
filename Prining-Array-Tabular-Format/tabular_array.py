81d9addfa5c392e8d3c48bf4d500aa5f87e44fdf
Hadi <hadi.alnehlawi@gmail.com>
2020-07-03 23:41:24 +0400

    almost ccomplete the tabular challenge

--- Prining-Array-Tabular-Format/tabular_array.py
+++ Prining-Array-Tabular-Format/tabular_array.py
@@ -1,40 +1,85 @@
-def number_of_digit(number):
-    count=0
-    while(number>0):
-        count = count + 1
-        number = number // 10
-    return count
+class TabularArray():
 
-def width_of_cell(A):
-    max_A = max(A)
-    return number_of_digit(max_A)
+    def __init__(self,A,K):
+        self.A = A
+        self.K = K
 
 
-def number_of_rows(A,K):
-    if len(A) % K == 0:
-        return  len(A) // K
-    else:
-        return (len(A) // K) + 1
+    def number_of_digit(self,number):
+        count=0
+        while(number>0):
+            count = count + 1
+            number = number // 10
+        return count
+
+    def width_of_cell(self):
+        max_A = max(self.A)
+        return self.number_of_digit(max_A)
+
+    def number_of_rows(self):
+        if len(self.A) % self.K == 0:
+            return  len(self.A) // self.K
+        else:
+            return (len(self.A) // self.K) + 1
+
+    def number_of_lines(self):
+        return (2 * self.number_of_rows() ) + 1
 
 
-def convert_cell_to_list(A,number):
-    width = width_of_cell(A)
-    print(width)
-    result = [ "" for _ in range(width)]
-    x = number
-    str_numbers = []
-    while(x>0):
-        str_numbers.append(x % 10)
-        x = x // 10
-    revert_numbers = [x for x in str_numbers if x != ""]
-    revert_numbers = revert_numbers[::-1]
-    sub_start = len(result) - number_of_digit(number)
-    sub_end = len(result)
-    result[sub_start:sub_end] = revert_numbers
-    return result
+    def convert_cell_to_list(self,number):
+        width = self.width_of_cell()
+        result = [ " " for _ in range(width)]
+        x = number
+        str_numbers = []
+        while(x>0):
+            str_numbers.append(x % 10)
+            x = x // 10
+        revert_numbers = [x for x in str_numbers if x != ""]
+        revert_numbers = revert_numbers[::-1]
+        sub_start = len(result) - self.number_of_digit(number)
+        sub_end = len(result)
+        result[sub_start:sub_end] = revert_numbers
+        result.append("|")
+        result = [str(x) for x in result]
+        return result
 
 
+    def draw_number(self):
+        result = "|"
+        width = self.width_of_cell()
+        cell = ""
+        for i in range(1,self.K+1):
+            cell = "x" * width
+            cell += "|"
+            result += cell
+        return result
 
-A = [2,289,33,29,900280,3,22,4,100,9,8]
+    def draw_separator(self):
+        result = "+"
+        width = self.width_of_cell()
+        cell = ""
+        for i in range(1,self.K+1):
+            cell = "-" * width
+            cell += "+"
+            result += cell
+        return result
 
-print(convert_cell_to_list(A,280))
+    def draw(self):
+        result = ""
+        index = 0
+        for i in range(self.number_of_lines()):
+            if i % 2 == 0:
+                result += self.draw_separator()
+            else:
+                result +=  "|"
+                for j in range(self.K):
+                    if index < len(self.A):
+                        number_list = self.convert_cell_to_list(self.A[index])
+                        item = "".join(number_list)
+                        result += item
+                        index += 1
+                    else:
+                        break
+
+            result += "\n"
+        return result
---
+++ Prining-Array-Tabular-Format/test_tabular_array.py
@@ -0,0 +1,88 @@
+from tabular_array import *
+import unittest
+
+
+class TestTabularArray(unittest.TestCase):
+
+    def test_convert_cell_to_list_1(self):
+        A = [2,289,33,29,900280,3,22,4,100,9,8]
+        number = 280
+        K = None
+        tabular_array = TabularArray(A,K)
+        self.assertEqual(tabular_array.convert_cell_to_list(number),[' ', ' ', ' ', '2', '8', '0',"|"])
+
+    def test_convert_cell_to_list_2(self):
+        A = [2,289,33,29,900280,3,22,4,100,9,8]
+        number = 98700
+        K = None
+        tabular_array = TabularArray(A,K)
+        self.assertEqual(tabular_array.convert_cell_to_list(number), [' ', '9', '8', '7', '0', '0',"|"])
+
+    def test_convert_cell_to_list_3(self):
+        A = [2,289,33,29,900280,3,22,4,100,9,8]
+        number = 876
+        K = 6
+        tabular_array = TabularArray(A,K)
+        self.assertEqual(tabular_array.convert_cell_to_list(number), [' ', ' ', ' ','8', '7', '6', "|"])
+
+    def test_draw_row_1(self):
+        A = [2,289,33,29,90028,3,22,4,100,9,8]
+        K = 4
+        tabular_array = TabularArray(A,K)
+        self.assertEqual(tabular_array.draw_number(), "|xxxxx|xxxxx|xxxxx|xxxxx|")
+
+    def test_draw_row_2(self):
+        A = [2,289,33,29,9002,3,22,4,100,9,8]
+        K = 5
+        tabular_array = TabularArray(A,K)
+        self.assertEqual(tabular_array.draw_number(), "|xxxx|xxxx|xxxx|xxxx|xxxx|")
+
+    def test_draw_separator_1(self):
+        A = [2,289,33,29,9002,3,22,4,100,9,8]
+        K = 5
+        tabular_array = TabularArray(A,K)
+        self.assertEqual(tabular_array.draw_separator(), "+----+----+----+----+----+")
+
+    def test_draw_separato_2(self):
+        A = [2,289,33,29,90028,3,22,4,100,9,8]
+        K = 4
+        tabular_array = TabularArray(A,K)
+        self.assertEqual(tabular_array.draw_separator(), "+-----+-----+-----+-----+")
+
+    def test_number_of_lines(self):
+        A = [2,289,33,29,90028,3,22,4,100,9,8,22,43,53] # 14
+        K = 4
+        tabular_array = TabularArray(A,K)
+        self.assertEqual(tabular_array.number_of_lines(), 9)
+
+    def test_number_of_lines(self):
+        A = [2,289,33,29,90028,3,22,4,100,9,8,22,43] # 13
+        K = 3
+        tabular_array = TabularArray(A,K)
+        self.assertEqual(tabular_array.number_of_lines(), 11)
+
+    def test_number_of_lines(self):
+        A = [2,289,33,29,90028,3,22,4,100,9] # 10
+        K = 4
+        tabular_array = TabularArray(A,K)
+        self.assertEqual(tabular_array.number_of_lines(), 7)
+
+    def test_number_of_lines(self):
+        A = [2,289,33,29,90028,3,22,4,100,9] # 10
+        K = 5
+        tabular_array = TabularArray(A,K)
+        self.assertEqual(tabular_array.number_of_lines(), 5)
+
+    def test_draw(self):
+        A = [2,289,33,29,900,3,22,4,100,9] # 10
+        K = 4
+        tabular_array = TabularArray(A,K)
+        new_line = '\n'
+        actual_output = f"+-----+-----+{new_line}+-----+-----+-----+{new_line}+-----+-----+-----+"
+        self.assertEqual(tabular_array.draw(),"TEST" )
+    #
+
+
+
+if __name__ == "__main__":
+    unittest.main()
